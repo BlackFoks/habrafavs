13fcf3c667b6dd49be7eebb9fd8e15b5b46ccd34
blackfoks <mail@blackfoks.com>
2011-05-28 23:35:35 +0700

    init commit with empty app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habrafavs::Application.config.secret_token = '3cb28b25e596d6159e6fb3cb15b46a6281c207317ded8d5f39eb74f7cedad59c4684f221103e17f8f7234f9e04b1386e3be13eaba5263ecc9bc0975043e2db8c'
